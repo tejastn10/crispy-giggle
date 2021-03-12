631825b836a1e56e192f3781d489cc6cbb4e4677
Tejas Nikhar <tejastn10@gmail.com>
2021-03-13 03:20:38 +0530

    📦 UPDATE: Add Command

--- commands/add.go
+++ commands/add.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"crispy-giggle/database"
+
 	"fmt"
 	"strings"
 
@@ -13,6 +15,11 @@ var addCmd = &cobra.Command{
 	Short: "Crispy-giggle adds a task to your task list",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
+		_, err := database.CreateTask(task)
+		if err != nil {
+			fmt.Println("Something went Wrong!", err)
+			return
+		}
 		fmt.Printf("Added \"%s\" to your Task List. \n", task)
 	},
 }
