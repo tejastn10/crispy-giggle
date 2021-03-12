4bdc630097d14b1c0de51bc38bbcd0f02c007393
Tejas Nikhar <tejastn10@gmail.com>
2021-03-13 02:11:35 +0530

    🐛 FIX: Scope Bug

--- database/task.go
+++ database/task.go
@@ -18,7 +18,7 @@ type Task struct {
 // Init Fucntion for Bolt database
 func Init(dbPath string) error {
 	var err error
-	db, err := bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		return err
 	}
