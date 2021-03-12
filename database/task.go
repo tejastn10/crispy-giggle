b7c5b37d9fa76a077f9d9275c8e3b3da7ade52cd
Tejas Nikhar <tejastn10@gmail.com>
2021-03-13 03:19:08 +0530

    🧩 NEW: Createtask Function

--- database/task.go
+++ database/task.go
@@ -30,6 +30,22 @@ func Init(dbPath string) error {
 	})
 }
 
+// CreateTask Function to create a task
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
 func itob(v int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(v))
