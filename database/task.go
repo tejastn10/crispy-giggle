1051e196cff86164d259b8d8cc49db0028013e7f
Tejas Nikhar <tejastn10@gmail.com>
2021-03-13 03:20:05 +0530

    🧩 NEW: Get Task list Function

--- database/task.go
+++ database/task.go
@@ -46,6 +46,28 @@ func CreateTask(task string) (int, error) {
 	return id, nil
 }
 
+// GetTasks return Task List
+func GetTasks() ([]Task, error) {
+	var tasks []Task
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
 // DeleteTask to delete a task
 func DeleteTask(key int) error {
 	return db.Update(func(tx *bolt.Tx) error {
